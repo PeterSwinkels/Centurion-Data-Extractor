9f62c569f917d50aab5161895c3d82c1c4711727
Peter Swinkels <peter_swinkels@hotmail.com>
2024-09-02 17:26:16 +0200

    Add files via upload

--- Centurion Data Extractor/My Project/Settings.Designer.vb
+++ Centurion Data Extractor/My Project/Settings.Designer.vb
@@ -63,10 +63,10 @@ Namespace My
     Global.System.Runtime.CompilerServices.CompilerGeneratedAttribute()> _
    Friend Module MySettingsProperty
 
-      <Global.System.ComponentModel.Design.HelpKeywordAttribute("My.Settings")> _
-      Friend ReadOnly Property Settings() As Global.Centurion_Data_Extractor.My.MySettings
+      <Global.System.ComponentModel.Design.HelpKeywordAttribute("My.Settings")>
+      Friend ReadOnly Property Settings() As Global.CenturionDataExtractorProgram.My.MySettings
          Get
-            Return Global.Centurion_Data_Extractor.My.MySettings.Default
+            Return Global.CenturionDataExtractorProgram.My.MySettings.Default
          End Get
       End Property
    End Module
